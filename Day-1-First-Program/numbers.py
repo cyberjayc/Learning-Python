5b8d6891dd093e5cb4bdb2d154b4810f7103991a
Jaycob <saduraljaycobpersonal@gmail.com>
2024-09-17 20:20:34 -0700

    Add files via upload

---
+++ Day-1-First-Program/numbers.py
@@ -0,0 +1,42 @@
+# Fundamental Data Types
+
+# Integers
+# print("These are integers")
+# print(1 + 1)
+# print(2 - 1)
+# print(2 * 5)
+# print(2 / 4)
+
+# Floats
+# print("These are floats")
+# print(0.1 + 0.1)
+# print(0.2 - 0.1)
+# print(0.2 * 0.5)
+# print(0.2 / 0.4)
+
+# Performing the Type Function
+# Type is a function that returns the type of the object
+# number_int = 5
+# number_float = 2.3
+
+# print(type(number_int))
+# print(type(number_float))
+
+# Extra
+
+# To the power of
+# print(2**3)
+
+# Round it down
+# print(2 // 4)
+
+# Modulo - remainder of this division
+# print(5 % 4)
+
+# Math Functions
+# The round() function rounds the number
+print(round(3.5))
+
+# The abs() function returns the absolute value of the argument, it basically means no negative numbers
+# It changes the negative number to a positive number
+print(abs(8))
