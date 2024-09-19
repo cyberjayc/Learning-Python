62762ef6de060f2e2178eb9d1bde1b68634ff4e9
Jaycob <saduraljaycobpersonal@gmail.com>
2024-09-18 21:03:19 -0700

    Add files via upload

---
+++ Day-1-First-Program/variables.py
@@ -0,0 +1,15 @@
+# Variable is a container that stores a value
+question_one = "What is your first name?"
+question_two = "What is your last name?"
+first_name = input(question_one)
+last_name = input(question_two)
+print("Hello, " + first_name + " " + last_name)
+
+# Best practices for variables
+# snake_case
+# Start with lowercase or underscore
+# Letters, numbers, underscores
+# Case sensitive
+# Don't overwrite keywords
+# Constants should be in capitals
+# __ are dunder variables, dont create variables with two underscores
