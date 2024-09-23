32815075f3b6838f72a38a33491d9b0b1eeba887
Jaycob <saduraljaycobpersonal@gmail.com>
2024-09-23 14:33:25 -0700

    Add files via upload

---
+++ augmented_operator_assignment.py
@@ -0,0 +1,23 @@
+# Augmented Assignment Operator
+# Augmented Assignment Operator is an operator that performs an arithmatic operation and assigns the result to the variable.
+
+# Addition
+addition = 1
+addition += 1
+print(addition)
+
+# Multiplication
+multiplication = 5
+multiplication *= 5
+print(multiplication)
+
+# Subtraction
+subtraction = 8
+subtraction -= 5
+print(subtraction)
+
+# Division
+division = 10
+division /= 2
+print(division)
+
